9fe1242d27b2316a816ed3c3e0a049b14d32b4f2
Prasanth Raghavan <mailprasanthtr@gmail.com>
2017-09-20 06:25:29 +0530

    Full screen childs

--- src/app/components/Custom.js
+++ src/app/components/Custom.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Custom extends React.Component {
   render() {
     return (
-      <div className="custom">
+      <div className="custom full-height">
         <h1>Custom</h1>
       </div>
       );
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="home">
+      <div className="home full-height">
         <h1>Home</h1>
       </div>
       );
